cba7495d208e9b5ca965dd99010e2263cf6d5680
harshithur_ <115203695+harshithur@users.noreply.github.com>
2024-11-04 19:44:51 +0530

    Add files via upload

---
+++ JavaDay-03/Demo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+class Demo{
+public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+   
+  System.out.println("Enter your 2 number: ");
+  int a = sc.nextInt();
+  int b = sc.nextInt();
+
+  int c = a + b ;
+  System.out.println("Sum of 2 number are : "+c);
+
+
+ 
+  }
+}
