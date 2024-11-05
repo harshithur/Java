8e1aebf58bbfd2820e63fe743177d746e4fd25f0
harshithur_ <115203695+harshithur@users.noreply.github.com>
2024-11-05 17:35:07 +0530

    Add files via upload

---
+++ Airthmetic.java
@@ -0,0 +1,25 @@
+
+public class Airthmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int nums[] = {3,7,2,4};
+//		System.out.println(nums[1]);
+		
+		int nums[] = new int[4];
+		nums[0] = 1;
+		nums[1] = 2;
+		nums[2] = 3;
+		nums[3] = 4;
+		
+		for(int i=0;i<=3;i++) {
+			System.out.println(nums[i]);
+		}
+		
+//		System.out.println(nums[0]);
+//		System.out.println(nums[1]);
+//		System.out.println(nums[2]);
+//		System.out.println(nums[3]);
+		
+	}
+}
