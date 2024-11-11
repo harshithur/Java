3fbaee8311d0b0bf3055b98b7cb08f335ff87f2d
harshithur_ <115203695+harshithur@users.noreply.github.com>
2024-11-11 11:08:16 +0530

    Add files via upload

---
+++ JavaDay-10/Metheods.java
@@ -0,0 +1,75 @@
+// class Solution {
+
+//      public static void main(String[] args) {
+//           a1();
+//           a2();
+//      }
+
+//      static void a2() {
+//           System.out.println("I am writting metheods in java...12");
+//      }
+
+//      static void a1() {
+//           System.out.println("I am writting metheods in java...");
+//      }
+// }
+
+//====================================================================================
+//Meteods that not take any input parameters and not returning somthing......
+
+
+// class Solution {
+//      public static void main(String[] args) {
+//           hello();
+//      }
+
+//      // Meteods that not take any input parameters and not returning anything......
+//      static void hello() {
+//           System.out.println("Meteods that not take any input parameters and not returning somthing......");
+//      }
+
+// }
+
+
+//========================================================================================
+// Meteods that take inputs parameters and not returning anything......
+
+
+// class Solution{
+//      public static void main(String [] args){
+//         write(10, 02);
+//      }
+//      static void write(int a,int b){
+//             System.out.println("Meteods that take inputs parameters and not returning anything......");
+                      
+//      }
+
+// }
+
+//=========================================================================================
+// Meteods that doesn't take any inputs parameters and returning something......
+
+//  class Solution{
+//      public static void main(String [] args){
+//         write();
+//      }
+//      static int write(){
+//             System.out.println("Meteods that doesn't take any inputs parameters and returning something......");
+//             return 42;
+                      
+//      }
+
+// }
+//===========================================================================================
+// Meteods that take inputs parameters and  returning something......
+
+// class Solution{
+//      public static void main(String [] args){
+//         write(10, 02);
+//      }
+//      static int write(int a,int b){
+//             System.out.println("Meteods that take inputs parameters and  returning something......");
+//                    return a+b;   
+//      }
+
+// }
